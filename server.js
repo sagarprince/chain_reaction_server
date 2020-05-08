56cfddf4d365817e2cdbf77ebd40a398c862433a
sagarpansare <sagarprince2012@gmail.com>
2020-05-09 03:29:24 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,146 @@
+'use strict';
+
+function Server() {
+  this.rooms = {};
+  this.express = require('express');
+  this.app = this.express();
+  this.httpServer = require('http').Server(this.app);
+  this.io = require('socket.io')(this.httpServer);
+}
+
+Server.prototype.setConfiguration = function () {
+  this.app.set('port', process.env.PORT || 4545);
+};
+
+Server.prototype.start = function () {
+  this.httpServer.listen(this.app.get('port'));
+  console.log('Server Started On Port : ' + this.app.get('port'));
+  this.realTimeRoutes();
+};
+
+Server.prototype.realTimeRoutes = function () {
+  var self = this;
+  this.io.on('connection', function (socket) {
+    console.log('connected :- ', socket.id);
+
+    socket.on('create_game', function (data) {
+      self.createGame(socket, data);
+    });
+
+    socket.on('join_game', function (data) {
+      self.joinGame(socket, data);
+    });
+
+    socket.on('disconnect', function () {
+      console.log('disconnect...');
+    });
+  });
+};
+
+Server.prototype.createGame = function (socket, data) {
+  var roomId = this.generateRoomId();
+  this.rooms[roomId] = {};
+  this.rooms[roomId]['playersCount'] = data.playersCount;
+  this.rooms[roomId]['pTurnIndex'] = 0;
+  this.rooms[roomId]['players'] = Array();
+  this.rooms[roomId]['players'].push(data.player);
+  socket.join(roomId);
+  socket.emit('respond', {
+    roomId: roomId,
+  });
+  console.log(this.rooms);
+};
+
+Server.prototype.joinGame = function (socket, data) {
+  var roomId = data.roomId;
+  var payload = {};
+  if (this.isRoomExist(roomId)) {
+    if (!this.isRoomFull(roomId)) {
+      if (!this.isPlayerNameExist(roomId, data.player.name)) {
+        if (!this.isPlayerColorExist(roomId, data.player.color)) {
+          this.rooms[roomId]['players'].push(data.player);
+          socket.join(roomId);
+          payload = {
+            status: 'success',
+            roomId: roomId,
+            playersCount: this.rooms[roomId]['playersCount'],
+            players: this.rooms[roomId]['players'],
+          };
+        } else {
+          payload = {
+            status: 'error',
+            code: 'color_exist',
+            message: 'This color is already taken.',
+          };
+        }
+      } else {
+        payload = {
+          status: 'error',
+          code: 'name_exist',
+          message: 'This name is already taken.',
+        };
+      }
+    } else {
+      payload = {
+        status: 'error',
+        code: 'room_full',
+        message: 'Room is already full',
+      };
+    }
+  } else {
+    payload = {
+      status: 'error',
+      code: 'invalid_room_id',
+      message: 'Please enter a valid room code.',
+    };
+  }
+  socket.emit('respond', payload);
+  if (payload['status'] == 'success') {
+    socket.broadcast.to(roomId).emit('joined', {
+      roomId: roomId,
+      players: this.rooms[roomId]['players'],
+    });
+  }
+  console.log(this.rooms);
+};
+
+Server.prototype.isRoomExist = function (roomId) {
+  var room = this.rooms[roomId];
+  return !room ? false : true;
+};
+
+Server.prototype.isRoomFull = function (roomId) {
+  var room = this.rooms[roomId];
+  var playersCount = room['playersCount'];
+  var totalPlayers = room['players'].length;
+  return playersCount === totalPlayers ? true : false;
+};
+
+Server.prototype.isPlayerNameExist = function (roomId, name) {
+  var players = this.rooms[roomId]['players'];
+  var index = players.findIndex((p) => {
+    return (
+      p.name.toLowerCase().replace(/\s/g, '') ===
+      name.toLowerCase().replace(/\s/g, '')
+    );
+  });
+  return index > -1;
+};
+
+Server.prototype.isPlayerColorExist = function (roomId, color) {
+  var players = this.rooms[roomId]['players'];
+  var index = players.findIndex((p) => p.color === color);
+  return index > -1;
+};
+
+Server.prototype.generateRoomId = function () {
+  var roomId = Math.floor(10000 + Math.random() * 90000);
+  if (roomId in this.rooms) {
+    roomId = generateRoomId();
+  }
+  return roomId;
+};
+
+const server = new Server();
+server.setConfiguration();
+server.start();
